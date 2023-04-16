180354e43412c810be3c0db5d8e2d492a571491a
AndSim <siminiucandrew@gmail.com>
2023-04-16 14:59:54 +0300

    add random number, add basic game logic: if input number matches the random number, number is high, number is low and outputs a message

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high Number!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low Number!';
+  }
+});
