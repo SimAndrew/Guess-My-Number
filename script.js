9d1d8006265ed12d67235a7479e712cd429c5613
AndSim <siminiucandrew@gmail.com>
2023-04-16 20:02:18 +0300

    feat: add score logic: entered a wrong number score shown to decrease, add logic: when the score is zero, then outputs a message: You lost the game

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -13,8 +15,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high Number!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high Number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low Number!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low Number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
